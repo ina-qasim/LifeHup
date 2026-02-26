c650d4f380d996f0ef10830db059d813781058ff
Office caawiye <fadumoqasim873@gmail.com>
2026-02-26 13:44:56 +0300

    App.jsxt

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "./ThemeContex";
 
 const Navbar = () => {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
       <h2 className="logo">LifeHub</h2>
 
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/music">Music</Link>
-        <Link to="/books">Books</Link>
-        <Link to="/cart">Cart</Link>
+      {/* Hamburger */}
+      <div 
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+
+      <div className={`links ${menuOpen ? "show" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/movies" onClick={() => setMenuOpen(false)}>Movies</Link>
+        <Link to="/music" onClick={() => setMenuOpen(false)}>Music</Link>
+        <Link to="/books" onClick={() => setMenuOpen(false)}>Books</Link>
+        <Link to="/cart" onClick={() => setMenuOpen(false)}>Cart</Link>
+
         <button
-        className={`theme-toggle ${darkMode ? "dark" : ""}`}
-        onClick={() => setDarkMode(!darkMode)}
+          className={`theme-toggle ${darkMode ? "dark" : ""}`}
+          onClick={() => setDarkMode(!darkMode)}
         >
-        <div className="toggle-circle"></div>
+          <div className="toggle-circle"></div>
         </button>
-      
+      </div>
     </nav>
   );
 };
